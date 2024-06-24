3c4e756ec3ddc6a8d35127d2f615f458ef65aa5a
vsukumaran vsukumaran <vsukumaran@vanenburg.com>
2024-06-24 15:38:10 +0530

    Commit for the generated/updated files via system

---
+++ Frontend/base/app/auth/prototype.variables.ts
@@ -0,0 +1,5 @@
+export class PrototypeVariables {
+	public static APP_ID = "2f8823ea-1490-4549-ba9c-9a781a48cdff";
+	public static TENANT_ID = "99dc49fd-c8d1-4356-9b81-f896135ab021";
+	public static DESIGN_STUDIO_URL = "https://localhost/";
+}
